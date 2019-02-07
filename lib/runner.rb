6d3061596e8af6756304801c36394dd8b3ff3973
alevmaltsev <a.maltsev@restream.rt.ru>
2019-02-07 14:40:14 +0400

    make runner and write convert algorithm

---
+++ lib/runner.rb
@@ -0,0 +1,24 @@
+require_relative './converters/atom_converter'
+require_relative './converters/rss_converter'
+require_relative './parsers/atom_parser'
+require_relative './parsers/rss_parser'
+require_relative './readers/file_reader'
+require_relative './readers/url_reader'
+
+READERS = [FileReader, UrlReader]
+PARSERS = [RssParser, AtomParser]
+CONVERTERS = [RssConverter, AtomConverter]
+
+module Runner
+  def self.start(options, source)
+    reader = READERS.find{ |reader| reader.can_read?(source) }
+    feed = reader.nil? ? raise : reader.read(source)
+    parser = PARSERS.find{ |parser| parser.can_parse?(feed) }
+    data = parser.nil? ? raise : parser.parse(feed)
+    # updateData = Options.exist?(options) ? Options.updateData(data) : data
+    converter = CONVERTERS.find{ |converter| converter.can_convert?(options[:output]) }
+    converter.convert(data)
+  rescue StandardError => e 
+    puts 'Runner', e
+  end
+end
